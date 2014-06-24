323f2389649ce5cfbf97f14cb358faa34ca2450c
Glen Horton <hortongn@ucmail.uc.edu>
2014-06-24 15:28:10 -0400

    Allow resque to be started as different user

--- script/start_resque_at_boot.sh
+++ script/start_resque_at_boot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
 # Run this from /etc/rc.local at boot time
+# ex. /srv/apps/curate_uc/script/start_resque_at_boot.sh curate
 
-su vagrant -c 'export PATH=$PATH:/opt/fits/fits-0.6.2/; source /usr/local/rvm/scripts/rvm; cd /srv/apps/curate_uc/; ./script/restart_resque.sh development'
+su $1 -c 'export PATH=$PATH:/opt/fits/fits-0.6.2/; source /usr/local/rvm/scripts/rvm; cd /srv/apps/curate_uc/; ./script/restart_resque.sh development'
