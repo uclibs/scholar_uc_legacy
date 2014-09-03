be44fb6dedb2523eed25827eb0f48a8206ecaefd
Glen Horton <glen.horton@gmail.com>
2014-09-03 10:43:00 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'UC Digital Projects <ucdp@uc.edu>'
+  config.mailer_sender = 'Scholar@UC <scholar@uc.edu>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
