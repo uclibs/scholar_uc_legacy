35a7101b585c601eb87ea0aeb16701a72bc38249
Glen Horton <glen.horton@gmail.com>
2014-10-03 11:26:48 -0400

    Adjust check_resque_running.sh to restart workers

--- script/check_resque_running.sh
+++ script/check_resque_running.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Check if resque workers are running
+# Restart workers if they are not running
+# Usage: check_resque_running.sh <hostname> <development|production>
 
 if ps aux | grep -v grep | grep resque | grep 'Waiting for' > /dev/null 2>&1
   then
     sleep 1;
   else
-    echo "Hydra server $1 appears to have no resque workers running.  Run /srv/apps/curate_uc/script/restart_resque.sh to restart them." > /tmp/resquealert
+    echo "Hydra server $1 did not have resque workers running.  The workers have been automatically restarted and will be checked again in one hour." > /tmp/resquealert
     sleep 1
-    mail -s "Scholar resque workers not running on $1" scholar@uc.edu < /tmp/resquealert
+    mail -s "Scholar resque workers restarted on $1" scholar@uc.edu < /tmp/resquealert
+    cd /srv/apps/curate_uc/script
+    /srv/apps/curate_uc/script/restart_resque.sh $2
 fi
 
