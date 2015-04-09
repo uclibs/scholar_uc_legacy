8c092a452734ac93527cf936fd9213bb0f214ab8
Glen Horton <hortongn@ucmail.uc.edu>
2015-04-09 13:26:55 -0400

    Tweak specs for new layout

--- spec/views/shared/_site_actions.html.erb_spec.rb
+++ spec/views/shared/_site_actions.html.erb_spec.rb
@@ -7,16 +7,8 @@ describe 'shared/_site_actions.html.erb' do
     render partial: 'shared/site_actions.html.erb'
   end
 
-  def have_login_section
-    have_tag('.login', with: { href: new_user_session_path } )
-  end
-
   def have_add_content_section(&block)
     have_tag('.add-content', &block)
-  end
-
-  def have_my_actions_section(&block)
-    have_tag('.my-actions', &block)
   end
 
   def have_about_section(&block)
@@ -35,8 +27,7 @@ describe 'shared/_site_actions.html.erb' do
     let(:person) { double(pid: 'test:1234') }
     let(:name) { 'My Display Name' }
     let(:current_user) { User.new(name: name, person: person).tap {|u| u.stub(groups: ['registered'])} }
-    it 'renders a link to create a new user session' do
-      expect(rendered).to_not have_login_section
+    it 'renders the add content and menu buttons' do
       expect(rendered).to have_add_content_section do
         with_tag '.quick-create' do
           with_tag 'a.link-to-full-list', with: { href: new_classify_concern_path }
@@ -44,30 +35,19 @@ describe 'shared/_site_actions.html.erb' do
           with_tag 'a.new-collection', with: { href: new_collection_path }, text: 'Add a Collection'
         end
       end
-      expect(rendered).to have_my_actions_section do
-        with_tag '.my-actions' do
-          with_tag 'a.user-display-name', text: /#{name}/
-          with_tag '.dropdown-menu' do
-            with_tag 'a.my-works'
-            with_tag 'a.my-collections', with: { href: collections_path}
-            with_tag 'a.my-account', with: { href: user_profile_path }
-            with_tag 'a.my-proxies', with: { href: person_depositors_path('1234') }
-            with_tag 'a.log-out', with: { href: destroy_user_session_path }
-          end
-        end
+      expect(rendered).to have_about_section
+      expect(rendered).to have_help_section
+      expect(rendered).to have_browse_section
       end
-    end
   end
 
   context 'not logged in' do
     let(:current_user) { nil }
-    it 'renders a link to create a new user session' do
+    it 'renders the menu buttons' do
       expect(rendered).to_not have_add_content_section
-      expect(rendered).to_not have_my_actions_section
-      expect(rendered).to have_login_section
       expect(rendered).to have_about_section
       expect(rendered).to have_help_section
       expect(rendered).to have_browse_section
-    end
+  end
   end
 end
