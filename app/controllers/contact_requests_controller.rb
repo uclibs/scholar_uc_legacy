67af4a83ae99e314b11b655c0e332ba56af75146
Lukas Miciunas <miciunla@mail.uc.edu>
2015-05-29 12:35:50 -0400

    Add re-captcha to contact form to prevent spamming

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -1,15 +1,27 @@
 class ContactRequestsController < ApplicationController
   SUCCESS_NOTICE = "Thank you.  Your message has been sent to the Scholar@UC team."
+  FAIL_NOTICE = "You must complete the Captcha to confirm the form."
 
   def new
+  end
+
+  def verify_google_recptcha(bamboo_secret_key,response)
+    status = `curl "https://www.google.com/recaptcha/api/siteverify?secret=#{bamboo_secret_key}&response=#{response}"`
+    hash = JSON.parse(status)
+    hash["success"] == true ? true : false
   end
 
   def create
     name = params[:name]
     email = params[:email]
     comments = params[:comments]
-    NotificationMailer.notify(name,email,comments).deliver
-    redirect_to catalog_index_path, notice: SUCCESS_NOTICE
-  end
+    status = verify_google_recptcha(bamboo_secret_key,params["g-recaptcha-response"])
 
+    if (status)
+      NotificationMailer.notify(name,email,comments).deliver
+      redirect_to catalog_index_path, notice: SUCCESS_NOTICE
+    else
+      redirect_to contact_requests_path, notice: FAIL_NOTICE
+    end
+  end
 end
