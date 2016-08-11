db79d002a478f0b284e1b7bab20c5fbec7908bc2
Glen Horton <hortongn@ucmail.uc.edu>
2016-08-11 21:34:07 +0000

    Allow Shibboleth attribute updates if never signed in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,24 @@
 FactoryGirl.define do
-  factory :shibboleth_user, class: 'User' do
+  factory :shibboleth_person, class: Person do
+    depositor 'fake.user@uc.edu'
     email 'fake.user@uc.edu'
+  end
+
+  factory :shibboleth_profile, class: Profile do
+    depositor 'fake.user@uc.edu'
+  end
+
+  factory :shibboleth_user, class: 'User' do
+    ignore do
+      count 1
+      person_pid nil
+    end
+    email 'fake.user@uc.edu'
+    first_name 'Fake'
+    last_name 'User'
     password '12345678'
     password_confirmation '12345678'
+    sign_in_count {"#{count}"}
+    repository_id {"#{person_pid}"}
   end
 end
