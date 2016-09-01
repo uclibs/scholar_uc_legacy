34652d18c580f19a45453b5f58033dea94e7a34a
crowesn <crowesn@gmail.com>
2016-09-01 15:56:58 -0400

    add orcid

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,17 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+        config.omniauth(:orcid, Orcid.provider.id, Orcid.provider.secret,
+                        scope: Orcid.provider.authentication_scope,
+                        member: true,
+                        sandbox: true,
+                        client_options: {
+                          site: Orcid.provider.site_url,
+                          authorize_url: Orcid.provider.authorize_url,
+                          token_url: Orcid.provider.token_url
+                        }
+                        )
+      
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
