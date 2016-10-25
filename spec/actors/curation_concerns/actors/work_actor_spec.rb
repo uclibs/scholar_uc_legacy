6b9b9759886c56c046486734035429b4eb3ea2d5
crowesn <crowesn@gmail.com>
2016-10-25 08:38:06 -0400

    reinstate empty tests

---
+++ spec/actors/curation_concerns/actors/work_actor_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate curation_concerns:work Work`
+require 'rails_helper'
+
+describe CurationConcerns::Actors::WorkActor do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
