36078804e54d70b628841861478bd559403bb50f
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2016-10-31 15:30:21 -0400

    make rights field nonrepeatable when creating or editing works

--- app/forms/curation_concerns/work_form.rb
+++ app/forms/curation_concerns/work_form.rb
@@ -5,5 +5,13 @@ module CurationConcerns
   class WorkForm < Sufia::Forms::WorkForm
     self.model_class = ::Work
     self.terms += [:resource_type]
+
+    def self.multiple?(field)
+      if field.to_sym == :rights
+        false
+      else
+        super
+      end
+    end
   end
 end
