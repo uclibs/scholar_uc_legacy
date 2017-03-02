4b841df9a2e9578adb7e3737e8025921151aa9f1
Lukas Miciunas <miciunla@mail.uc.edu>
2017-03-02 14:00:20 +0000

    Add test seed rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+require 'seed_methods'
 
 class AddInitialObjects < ActiveRecord::Migration
+
+  rando_pass = Devise.friendly_token.first(8)
+  accounts = {'manydeposits@example.com' => 'Many Deposits', 'nodeposits@example.com' => 'No Deposits', 'manager@example.com' => 'Repository Manager', 'delegate@example.com' => 'Student Delegate'} 
+
+  accounts.each do | email, name |
+    unless User.where(email: email).exists?
+      SeedMethods.new_account(email, name, rando_pass)
+      puts "-- Test account created: #{email}, #{rando_pass}"
+    end
+  end
+
+  accounts.reject{ | email, name | email == 'nodeposits@example.com' }.each do | email, name |
+    10.times { |i| SeedMethods.new_image(email, name) }
+    10.times { |i| SeedMethods.new_document(email, name) }
+    10.times { |i| SeedMethods.new_article(email, name) }
+    10.times { |i| SeedMethods.new_dataset(email, name) }
+    10.times { |i| SeedMethods.new_genericwork(email, name) }
+    10.times { |i| SeedMethods.new_video(email, name) }
+  end
 
   def self.create_admin_role(id)
     admin = Role.create(name: "admin")
@@ -12,7 +32,7 @@ class AddInitialObjects < ActiveRecord::Migration
   def self.create_admin_account
     rando_pass = Devise.friendly_token.first(8)
     user = User.create email: 'admin@example.com', password: rando_pass
-    puts "\n\n\t\t*** ADMIN USER PASS: #{rando_pass} ***\n\n"
+    puts "\n\n\t\t*** ADMIN USER (admin@example.com) PASS: #{rando_pass} ***\n\n"
     user.save
     user.id
   end
---
+++ lib/seed_methods.rb
@@ -0,0 +1,165 @@
+# frozen_string_literal: true
+module SeedMethods
+  def self.new_article(email, name)
+    a = Article.create(
+      title: ['This is the title'],
+      description: ['This is the abstract'],
+      depositor: email,
+      owner: email,
+      creator: [name],
+      subject: populate_subject,
+      publisher: ['Penguin Publishing'],
+      issn: [123_456_789],
+      language: ['English'],
+      based_near: ['The world'],
+      time_period: ['Now'],
+      bibliographic_citation: ['This is the bibliographic citation'],
+      required_software: 'This requires software',
+      note: 'This is the note',
+      date_created: [populate_date_created]
+    )
+    a.read_groups = [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC]
+    a.edit_users = [email]
+    a.save
+  end
+
+  def self.new_image(email, name)
+    a = Image.create(
+      title: ['This is the title'],
+      description: ['This is the description'],
+      depositor: email,
+      owner: email,
+      creator: [name],
+      subject: populate_subject,
+      publisher: ['Penguin Publishing'],
+      geo_subject: ['The world'],
+      time_period: ['Now'],
+      bibliographic_citation: ['This is the bibliographic citation'],
+      source: ['This is the source'],
+      required_software: 'This requires software',
+      date_created: [populate_date_created]
+    )
+    a.read_groups = [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC]
+    a.edit_users = [email]
+    a.save
+  end
+
+  def self.new_document(email, name)
+    a = Document.create(
+      title: ['This is the title'],
+      description: ['This is the description'],
+      depositor: email,
+      owner: email,
+      creator: [name],
+      subject: populate_subject,
+      publisher: ['Penguin Publishing'],
+      language: ['English'],
+      required_software: 'This requires software',
+      bibliographic_citation: ['This is the bibliographic citation'],
+      date_created: [populate_date_created]
+    )
+    a.read_groups = [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC]
+    a.edit_users = [email]
+    a.save
+  end
+
+  def self.new_dataset(email, name)
+    a = Dataset.create(
+      title: ['This is the title'],
+      description: ['This is the description'],
+      depositor: email,
+      owner: email,
+      creator: [name],
+      subject: populate_subject,
+      publisher: ['Penguin Publishing'],
+      alternate_title: ['This is the alternate title'],
+      language: ['English'],
+      based_near: ['The world'],
+      time_period: ['Now'],
+      bibliographic_citation: ['This is the bibliographic citation'],
+      required_software: 'This requires software',
+      note: 'This is the note',
+      date_created: [populate_date_created]
+    )
+    a.read_groups = [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC]
+    a.edit_users = [email]
+    a.save
+  end
+
+  def self.new_genericwork(email, name)
+    a = GenericWork.create(
+      title: ['This is the title'],
+      description: ['This is the description'],
+      depositor: email,
+      owner: email,
+      creator: [name],
+      subject: populate_subject,
+      publisher: ['Penguin Publishing'],
+      alternate_title: ['This is the alternate title'],
+      language: ['English'],
+      based_near: ['The world'],
+      time_period: ['Now'],
+      bibliographic_citation: ['This is the bibliographic citation'],
+      required_software: 'This requires software',
+      note: 'This is the note',
+      date_created: [populate_date_created]
+    )
+    a.read_groups = [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC]
+    a.edit_users = [email]
+    a.save
+  end
+
+  def self.new_video(email, name)
+    a = Video.create(
+      title: ['This is the title'],
+      description: ['This is the description'],
+      depositor: email,
+      owner: email,
+      creator: [name],
+      subject: populate_subject,
+      publisher: ['Penguin Publishing'],
+      alternate_title: ['This is the alternate title'],
+      language: ['English'],
+      based_near: ['The world'],
+      time_period: ['Now'],
+      bibliographic_citation: ['This is the bibliographic citation'],
+      required_software: 'This requires software',
+      note: 'This is the note',
+      date_created: [populate_date_created]
+    )
+    a.read_groups = [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC]
+    a.edit_users = [email]
+    a.save
+  end
+
+  def self.new_account(email, name, pass)
+    user = User.create(email: email, first_name: first_name(name), last_name: last_name(name), password: pass, password_confirmation: pass)
+    user.save!
+  end
+
+  def self.first_name(name)
+    name.split(" ")[0]
+  end
+
+  def self.last_name(name)
+    name.split(" ")[1]
+  end
+
+  # this method populates the subject field of a work with a random array of 2-3 elements
+  # the variation in elements in the array is intentional as any work type should be able
+  # to support a variable number of elements
+  def self.populate_subject
+    sample_subjects = ['geography', 'history', 'chemistry', 'research', 'biology', 'technology']
+    ret = []
+    3.times do
+      sample_subject = sample_subjects.sample
+      ret.push sample_subject unless ret.include?(sample_subject)
+    end
+    ret
+  end
+
+  # generates random date from epoch to now as string
+  def self.populate_date_created
+    Time.zone.at(rand * Time.now.to_i).to_s.sub(/\s(.*)/, '')
+  end
+end
