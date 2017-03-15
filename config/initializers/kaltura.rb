ab1e821c335d28224e14c0d41647621222ba09d4
crowesn <crowesn@gmail.com>
2017-03-15 09:20:54 -0400

    Integrate kaltura

---
+++ config/initializers/kaltura.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+Kaltura.configure do |config|
+  config.partner_id = 'bamboo_browse_everything_kaltura_id'
+  config.administrator_secret = 'bamboo_browse_everything_kaltura_secret'
+  config.service_url = 'https://www.kaltura.com'
+end
