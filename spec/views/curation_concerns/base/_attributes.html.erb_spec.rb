85053ebbc99ef3ddcb5d2cdfcff8b449cef0b36d
Lukas Miciunas <miciunla@mail.uc.edu>
2017-04-26 17:18:31 +0000

    Adds submitter to work show page

--- spec/views/curation_concerns/base/_attributes.html.erb_spec.rb
+++ spec/views/curation_concerns/base/_attributes.html.erb_spec.rb
@@ -5,6 +5,7 @@ describe 'curation_concerns/base/_attributes.html.erb' do
   let(:college) { 'Libraries' }
   let(:department) { 'Digital Repositories' }
   let(:related_url) { 'http://www.uc.edu' }
+  let(:submitter) { FactoryGirl.create(:user) }
 
   let(:solr_document) { SolrDocument.new(attributes) }
   let(:attributes) do
@@ -12,7 +13,8 @@ describe 'curation_concerns/base/_attributes.html.erb' do
       Solrizer.solr_name('has_model', :symbol) => ["GenericWork"],
       college_tesim: college,
       department_tesim: department,
-      related_url_tesim: related_url
+      related_url_tesim: related_url,
+      depositor_tesim: submitter.email
     }
   end
   let(:ability) { nil }
@@ -31,5 +33,6 @@ describe 'curation_concerns/base/_attributes.html.erb' do
     expect(rendered).to have_link(college, href: search_catalog_path('f[college_sim][]': college))
     expect(rendered).to have_link(department, href: search_catalog_path('f[department_sim][]': department))
     expect(rendered).to have_link(related_url)
+    expect(rendered).to have_link(href: sufia.profile_path(submitter))
   end
 end
