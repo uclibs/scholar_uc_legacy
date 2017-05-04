173e71137c720d66b19f2ffe666e26e8d4b0219e
Lukas Miciunas <miciunla@mail.uc.edu>
2017-05-04 14:50:57 +0000

    Display geo subject on all work show pages

--- app/presenters/image_presenter.rb
+++ app/presenters/image_presenter.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 class ImagePresenter < Sufia::WorkShowPresenter
-  delegate :alt_description, :alt_date_created, :college, :department, :alternate_title, :date_photographed, :genre, :time_period, :required_software, :note, :cultural_context, :doi, to: :solr_document
+  delegate :alt_description, :alt_date_created, :college, :department, :alternate_title, :date_photographed, :genre, :time_period, :required_software, :note, :geo_subject, :cultural_context, :doi, to: :solr_document
 end
