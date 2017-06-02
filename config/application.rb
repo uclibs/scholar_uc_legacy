c258d1ecb5d13ba94d16a70f82f46356f721f7ab
James Van Mil <james.vanmil@gmail.com>
2017-06-02 14:22:43 -0400

    Hyrax migration: Restore config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,24 @@ Bundler.require(*Rails.groups)
 
 module ScholarUc
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, spec: true
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += %W(#{config.root}/app/presenters/concerns)
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.exceptions_app = self.routes
+
+    config.application_root_url = 'http://localhost:3000'
+    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('services')
+    config.autoload_paths << Rails.root.join('jobs')
   end
 end
