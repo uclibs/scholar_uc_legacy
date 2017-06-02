c6359cea8b4ac17519a431fa5fdc4af1e211a4df
Glen Horton <hortongn@ucmail.uc.edu>
2017-06-02 16:49:40 +0000

    Fix editing of files

--- app/controllers/concerns/hyrax/file_sets_controller_behavior.rb
+++ app/controllers/concerns/hyrax/file_sets_controller_behavior.rb
@@ -3,7 +3,11 @@ require Hyrax::Engine.root.join('app/controllers/concerns/hyrax/file_sets_contro
 module Hyrax
   module FileSetsControllerBehavior
     def show
-      super
+      respond_to do |wants|
+        wants.html { presenter }
+        wants.json { presenter }
+        additional_response_formats(wants)
+      end
       permalink_message = "Permanent link to this page"
       @permalinks_presenter = PermalinksPresenter.new(main_app.common_object_path, permalink_message)
     end
