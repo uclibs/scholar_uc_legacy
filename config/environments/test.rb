8ef39e6a7d5f80a9a6ec8339783b607d80698d0a
Thomas Scherz <thomas.scherz@uc.edu>
2017-06-07 10:56:13 -0400

    Hyrax migration: Fix file_sets initializer for UV.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
+  # Needed for IIIF Manifest
+  config.action_mailer.default_url_options = { host: "test.host" }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
