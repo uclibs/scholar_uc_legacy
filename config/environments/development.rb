5119020d638c2514e740db49221dd9478451290d
Thomas Scherz <thomas.scherz@uc.edu>
2017-06-08 15:55:39 -0400

    Includes DisplaysImage in FileSetPresenters.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: "localhost:3001" }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -53,5 +55,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.active_job.queue_adapter = :inline
+  # config.active_job.queue_adapter = :inline
 end
