d2f2b754ef66514d3644f00b6274e4d71036b52c
Glen Horton <hortongn@ucmail.uc.edu>
2017-06-27 14:06:46 +0000

    Hyrax migration: Fix undefined work_type in specs

--- spec/features/virus_scan_spec.rb
+++ spec/features/virus_scan_spec.rb
@@ -21,7 +21,7 @@ describe 'Adding an infected file', js: true do
       expect(page).to have_content "Add files"
       attach_file("files[]", File.dirname(__FILE__) + "/../../spec/fixtures/image.jp2", visible: false)
       click_link "Metadata" # switch tab
-      title_element = find_by_id("#{work_type}_title")
+      title_element = find_by_id("generic_work_title")
       title_element.set("My Test Work")
       fill_in('Creator', with: 'Test User')
       college_element = find_by_id("generic_work_college")
