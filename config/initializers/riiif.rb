17e50c53713ef0ec9941736e0d60a460fc4d55cd
Thomas Scherz <thomas.scherz@uc.edu>
2017-08-07 17:10:31 -0400

    Adds URI.decode to remove slashes in RIIIF url.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -6,7 +6,7 @@ Riiif::Image.info_service = lambda do |id, _file|
   # but we just want the id for the FileSet it's attached to.
 
   # Capture everything before the first slash
-  fs_id = id.sub(/\A([^\/]*)\/.*/, '\1')
+  fs_id = URI.decode(id).sub(/\A([^\/]*)\/.*/, '\1')
   resp = ActiveFedora::SolrService.get("id:#{fs_id}")
   doc = resp['response']['docs'].first
   raise "Unable to find solr document with id:#{fs_id}" unless doc
