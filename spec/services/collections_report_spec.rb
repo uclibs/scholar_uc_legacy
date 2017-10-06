09bbbb1f65023d4c06d0e445c1eec68c6a62ae2f
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2017-10-06 17:23:31 +0000

    rubocop changes

--- spec/services/collections_report_spec.rb
+++ spec/services/collections_report_spec.rb
@@ -1,35 +1,36 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe CollectionsReport do
   describe '#report_location' do
-    it 'should be vendor/collections_report.csv' do
-      expect(CollectionsReport.report_location).to eq("#{Rails.root}/vendor/collections_report.csv")
+    it 'is vendor/collections_report.csv' do
+      expect(described_class.report_location).to eq("#{Rails.root}/vendor/collections_report.csv")
     end
   end
 
   describe '#create_report' do
-    let(:fake_collections) { [ 
+    let(:fake_collections) { [
       FakeCollection.new('pid', 'title', 'foo@bar.org', ['foo@bar.org'], ['pid', 'pid']),
       FakeCollection.new('pid', 'title', 'foo@bar.org', ['foo@bar.org'], ['pid']),
       FakeCollection.new('pid', 'title', 'foo@bar.org', ['foo@bar.org'], ['']),
-      FakeCollection.new('pid', 'title', 'foo@bar.org', ['foo@bar.org'], ['pid', 'pid']) 
+      FakeCollection.new('pid', 'title', 'foo@bar.org', ['foo@bar.org'], ['pid', 'pid'])
     ] }
 
-    before(:each) do
+    before do
       Collection.stub(:all).and_return(fake_collections)
 
-      File.delete(CollectionsReport.report_location) if File.exist?(CollectionsReport.report_location)
+      File.delete(described_class.report_location) if File.exist?(described_class.report_location)
     end
 
-    it 'should create a report in the report_location' do
-      CollectionsReport.create_report
-      expect(File).to exist(CollectionsReport.report_location) 
+    it 'creates a report in the report_location' do
+      described_class.create_report
+      expect(File).to exist(described_class.report_location)
     end
 
-    it 'should create a report one line longer than the number of objects reported on' do
-      CollectionsReport.create_report
+    it 'creates a report one line longer than the number of objects reported on' do
+      described_class.create_report
       expect(
-        File.open(CollectionsReport.report_location).readlines.size
+        File.open(described_class.report_location).readlines.size
       ).to eq(fake_collections.length + 1)
     end
 
--- spec/services/users_report_spec.rb
+++ spec/services/users_report_spec.rb
@@ -1,35 +1,36 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe UsersReport do
   describe '#report_location' do
-    it 'should be vendor/users_report.csv' do
-      expect(UsersReport.report_location).to eq("#{Rails.root}/vendor/users_report.csv")
+    it 'is vendor/users_report.csv' do
+      expect(described_class.report_location).to eq("#{Rails.root}/vendor/users_report.csv")
     end
   end
 
   describe '#create_report' do
-    let(:fake_users) { [ 
+    let(:fake_users) { [
       FakeUser.new('id1', 'foo@bar.com', 'Foo Bar', 'Foo', 'Bar'),
       FakeUser.new('id1', 'foo@bar.com', 'Foo Bar', 'Foo', 'Bar'),
       FakeUser.new('id1', 'foo@bar.com', 'Foo Bar', 'Foo', 'Bar'),
       FakeUser.new('id1', 'foo@bar.com', 'Foo Bar', 'Foo', 'Bar')
     ] }
 
-    before(:each) do
+    before do
       User.stub(:all).and_return(fake_users)
 
-      File.delete(UsersReport.report_location) if File.exist?(UsersReport.report_location)
+      File.delete(described_class.report_location) if File.exist?(described_class.report_location)
     end
 
-    it 'should create a report in the report_location' do
-      UsersReport.create_report
-      expect(File).to exist(UsersReport.report_location) 
+    it 'creates a report in the report_location' do
+      described_class.create_report
+      expect(File).to exist(described_class.report_location)
     end
 
-    it 'should create a report one line longer than the number of objects reported on' do
-      UsersReport.create_report
+    it 'creates a report one line longer than the number of objects reported on' do
+      described_class.create_report
       expect(
-        File.open(UsersReport.report_location).readlines.size
+        File.open(described_class.report_location).readlines.size
       ).to eq(fake_users.length + 1)
     end
 
