d06ce6e58e8dac985d50f20af85030f44ad927ca
Jayanth Dungavath <jayanthdeejay@gmail.com>
2017-10-23 12:42:57 -0400

    Fedora export script

--- script/exportFiles.pl
+++ script/exportFiles.pl
@@ -2,107 +2,153 @@
 use strict;
 use warnings;
 use 5.008;
+use Data::Dumper qw(Dumper);
+use Switch;
+
 
 #*******************************************************************************************
-# 1. Set FEDORA_HOME to Fedora install path (Where Fedora binaries are located)
-#       export FEDORA_HOME="/opt/rails-apps/curate_app/jetty/fedora"
-# 2. Execute above command before running this script
+# 1. Locate and set $datastreamPath and $objectStorePath variables accordingly.
+# 2. Create a directory where you want to export files and gets its absolute path.
 # 3. Extract PIDs of the files that you intend to extract in the form sufia:9z903118b and
-#       save them in a file. Update $PIDFile accordingly.
-# 4. Locate datastreamStore and update $datastreamPath accordingly.
-# 5. Set $fedoraBin such that it is $FEDORA_HOME/client/bin
-# 6. Create a directory where you want to export files and set $exportPath with the
-#       absolute path of the directory that you just created
-# 7. Set $port, $adminUser, $adminPass to Port, UID and Password of Fedora
-# 8. Add & like "perl exportFiles.pl &" if you want to run this script in the background
+#       save them in a file.
 #******************************************************************************************
 
 
-my $PIDFile = "PIDs.txt";
 my $datastreamPath = "/opt/rails-apps/curate_app/jetty/fedora/default/data/datastreamStore";
-my $fedoraBin = "/opt/rails-apps/curate_app/jetty/fedora/client/bin";
-my $exportPath = "/home/webapp/test";
-my $port = 4001;
-my $adminUser = "fedoraAdmin";
-my $adminPass = "fedoraAdmin";
+my $objectStorePath = "/opt/rails-apps/curate_app/jetty/fedora/default/data/objectStore";
+my $PIDFile;
+my $exportPath;
+my %metadata;
+my @script;
+my $scriptName='';
 
 
-my @PIDandFileVersion;
-my $start = 0.0;
-my $end = 0.0;
-my $total = 0.0;
-my $pidCount = 0.0;
+print "\n\n\nIf you pass any argument to the script like 'perl exportFiles.pl jay', script will not compute checksums.\n".
+      "1. Selecting option 1 to generate the export script will not export files. Only export script is generated.\n".
+      "2. Option 2 will both generate export script and will export files.\n".
+      "3. Option 3 will save the hash generated in this script to a file.\n\n";
+
+print "PID file: ";
+$PIDFile = <STDIN>;
+chomp $PIDFile;
+
+print "Export files to: ";
+$exportPath = <STDIN>;
+chomp $exportPath;
 
 
 open(my $inputFile, $PIDFile) or die $!;
 while(my $PID = <$inputFile>) {
-        chomp $PID;
-        $PIDandFileVersion[$.-1][0] = $PID;
-        my $getVersionCmd = 'sh '.$fedoraBin.'/fedora-dsinfo.sh localhost '.$port.' '.$adminUser.' '.$adminPass.' '.$PID.' http | grep VERSION | grep content';
-        $start = time;
-        my $fileVersion = `$getVersionCmd`;
-        $end = time;
-        $total += $end - $start;
-        chomp $fileVersion;
-        $fileVersion = (split(/\n/, $fileVersion))[0];
-        $fileVersion = (split(/:/, $fileVersion))[1];
-        $fileVersion = (split(/\./, $fileVersion))[1];
-        $PIDandFileVersion[$.-1][1] = $fileVersion;
-        $pidCount += 1;
-}
+    chomp $PID;
+    my $cmd = 'find '.$objectStorePath.' -name "*'.(split /:/, $PID)[1].'*" -print0 | xargs -0 grep "datastreamVersion ID=\"content." | wc -l';
+    chomp($metadata{$PID}{"curVer"} = (`$cmd`)-1);
+    my $ver = $metadata{$PID}{"curVer"};
+    while($ver >= 0) {
 
-
-my $logFile = $exportPath.'/'.'export.log';
-open (my $log, '>>', $logFile) or die $!;
-foreach my $row (@PIDandFileVersion) {
-        `mkdir $exportPath/@$row[0]`;
-        my $ver = @$row[1];
-        while($ver > -1) {
-                `mkdir $exportPath/@$row[0]/$ver`;
-                my $partialPID = (split(/:/,@$row[0]))[1];
-                my $findFileCmd = 'find '.$datastreamPath.' -name "*'.$partialPID.'*content*'.$ver.'" -print';
-                my $exportFile = `$findFileCmd`;
-                chomp $exportFile;
-                my $copyCMD = 'cp -p '.$exportFile.' '.$exportPath.'/'.@$row[0].'/'.$ver.'/';
-                chomp $copyCMD;
-                `$copyCMD`;
-
-                my $newChecksum = 'md5sum '.$exportFile;
-                $newChecksum = `$newChecksum`;
-                $newChecksum = (split(/ /, $newChecksum))[0];
-                chomp $newChecksum;
-
-                my $oldChecksum = 'find '.$datastreamPath.' -name "*'.$partialPID.'*characterization*'.$ver.'" -exec grep \'md5checksum\' \{\} \;';
-                $oldChecksum = `$oldChecksum`;
-                $oldChecksum = (split(/>/, $oldChecksum))[1];
-                $oldChecksum = (split(/</, $oldChecksum))[0];
-                chomp $oldChecksum;
-                if($oldChecksum eq $newChecksum) {
-                        print $log @$row[0];
-                        print $log " PASS\n";
-                }
-                else {
-                        print $log @$row[0];
-                        print $log " FAIL\n";
-                }
-
-                my $md5file1 = $exportPath.'/'.@$row[0].'/'.$ver.'/'.'oldChecksum.md5';
-                my $md5file2 = $exportPath.'/'.@$row[0].'/'.$ver.'/'.'newChecksum.md5';
-                open (my $md51, '>', $md5file1) or die $!;
-                open (my $md52, '>', $md5file2) or die $!;
-                print $md51 $oldChecksum;
-                close $md51;
-                print $md52 $newChecksum;
-                close $md52;
-                $ver--;
+        $cmd = 'find '.$datastreamPath.' -name "*'.(split /:/, $PID)[1].'*content*'.$ver.'" -print';
+        chomp($metadata{$PID}{"version.".$ver}{"path"} = `$cmd`);
+        if(!@ARGV) {
+            $cmd = 'md5sum '.`$cmd`;
+            $cmd = `$cmd`;
+            $cmd = (split/ /, $cmd)[0];
+            chomp($metadata{$PID}{"version.".$ver}{"checksum.new"} = $cmd);
         }
+        $cmd = 'find '.$datastreamPath.' -name "*'.(split /:/, $PID)[1].'*characterization*'.$ver.'" -print0 | xargs -0 grep "filename"';
+        chomp($cmd = `$cmd`);
+        chomp($metadata{$PID}{"version.".$ver}{"name"} = (split /</, (split />/, $cmd)[1])[0]);
+
+        $cmd = 'find '.$datastreamPath.' -name "*'.(split /:/, $PID)[1].'*characterization*'.$ver.'" -print0 | xargs -0 grep "md5checksum"';
+        chomp($cmd = `$cmd`);
+        chomp($metadata{$PID}{"version.".$ver}{"checksum.old"} = (split /</, (split />/, $cmd)[1])[0]);
+
+        $ver--;
+    }
 }
 close $inputFile;
-close $log;
 
-print 'Total time to extract '.$pidCount.' PID Versions: '.$total.' secs';
-print "\n";
-print 'Average time to extract each PID Version: ';
-printf("%.3f",($total/$pidCount));
-print ' secs';
-print "\n";
+my $input = '';
+while($input ne '4') {
+
+    clear_screen();
+
+    print "1. Generate export script\n".
+          "2. Export files\n".
+          "3. Dump hash to file\n".
+          "4. exit\n";
+    print ">>> ";
+    $input = <STDIN>;
+    chomp($input);
+
+    switch($input) {
+        case '1' {
+            genScript();
+        }
+        case '2' {
+            export();
+        }
+        case '3' {
+            hashDump();
+        }
+    }
+}
+
+exit(0);
+
+sub hashDump {
+    my $fName;
+    print "Save hash to: ";
+    $fName = <STDIN>;
+    chomp($fName);
+    $fName = $exportPath.'/'.$fName;
+    open(my $dump, '>>', $fName) or die $!;
+    print $dump Dumper( \%metadata );
+    close $dump;
+}
+
+sub clear_screen
+{
+    system("clear");
+}
+
+sub genScript {
+    print "Save export script as[*.sh]: ";
+    $scriptName = <STDIN>;
+    chomp($scriptName);
+    my $index = 0;
+    $script[$index] = '#!'.`which sh`;
+    for my $PID (keys(%metadata)) {
+        $script[++$index] = 'mkdir '.$exportPath.'/'.$PID;
+        my $rec = %metadata->{$PID};
+        my $ver = $rec->{curVer};
+        while($ver >= 0) {
+            my $subrec = $rec->{'version.'.$ver};
+            $script[++$index] = 'mkdir '.$exportPath.'/'.$PID.'/'.$ver;
+            $script[++$index] = 'cp -p "'.$subrec->{path}.'" "'.$exportPath.'/'.$PID.'/'.$ver.'/'.$subrec->{name}.'"';
+            $script[++$index] = 'echo '.$subrec->{'checksum.old'}.' >> '.$exportPath.'/'.$PID.'/'.$ver.'/oldChecksum.md5';
+            if(!@ARGV) {
+                $script[++$index] = 'echo '.$subrec->{'checksum.new'}.' >> '.$exportPath.'/'.$PID.'/'.$ver.'/newChecksum.md5';
+            }
+            $ver--;
+        }
+    }
+
+    $scriptName = $exportPath.'/'.$scriptName;
+    open(my $dump, '>>', $scriptName) or die $!;
+    foreach(@script) {
+        print $dump "$_\n";
+    }
+    close $dump;
+}
+
+
+sub export {
+    my $flag;
+    if($scriptName eq '') {
+        $flag = 1;
+    }
+    if($flag){
+        genScript();
+    }
+    my $cmd = 'sh '.$scriptName;
+    `$cmd`;
+}
