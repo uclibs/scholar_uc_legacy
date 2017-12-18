bfaa4c6cd143b40b8873727db1535a39bf440701
James Van Mil <james.vanmil@gmail.com>
2017-12-18 12:58:48 -0500

    (poorly) handle failed stat import attempts

--- app/services/hyrax/user_stat_importer.rb
+++ app/services/hyrax/user_stat_importer.rb
@@ -50,10 +50,10 @@ module Hyrax
         file_ids_for_user(user).each do |file_id|
           file = ::FileSet.find(file_id)
           view_stats = rescue_and_retry("Retried FileViewStat on #{user} for file #{file_id} too many times.") { FileViewStat.statistics(file, start_date, user.id) }
-          stats = tally_results(view_stats, :views, stats) unless view_stats.blank?
+          stats = tally_results(view_stats, :views, stats) unless (view_stats.blank? || view_stats)
           delay
           dl_stats = rescue_and_retry("Retried FileDownloadStat on #{user} for file #{file_id} too many times.") { FileDownloadStat.statistics(file, start_date, user.id) }
-          stats = tally_results(dl_stats, :downloads, stats) unless dl_stats.blank?
+          stats = tally_results(dl_stats, :downloads, stats) unless (dl_stats.blank? || dl_stats)
           delay
         end
       end
@@ -62,7 +62,7 @@ module Hyrax
         work_ids_for_user(user).each do |work_id|
           work = Hyrax::WorkRelation.new.find(work_id)
           work_stats = rescue_and_retry("Retried WorkViewStat on #{user} for work #{work_id} too many times.") { WorkViewStat.statistics(work, start_date, user.id) }
-          stats = tally_results(work_stats, :work_views, stats) unless work_stats.blank?
+          stats = tally_results(work_stats, :work_views, stats) unless (work_stats.blank? || work_stats)
           delay
         end
       end
