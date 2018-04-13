632b4b6a84e5fe1e0c4754cbcd1f0174885fc31a
Thomas Scherz <scherztc@ucmail.uc.edu>
2018-04-13 17:02:59 -0400

    Improves on work type presenter specs.

--- spec/presenters/etd_presenter_spec.rb
+++ spec/presenters/etd_presenter_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe EtdPresenter do
   subject { described_class.new(double, double) }
   it { is_expected.to delegate_method(:college).to(:solr_document) }
   it { is_expected.to delegate_method(:department).to(:solr_document) }
+  it { is_expected.to delegate_method(:title).to(:solr_document) }
+  it { is_expected.to delegate_method(:alternate_title).to(:solr_document) }
+  it { is_expected.to delegate_method(:geo_subject).to(:solr_document) }
+  it { is_expected.to delegate_method(:time_period).to(:solr_document) }
+  it { is_expected.to delegate_method(:required_software).to(:solr_document) }
+  it { is_expected.to delegate_method(:note).to(:solr_document) }
+  it { is_expected.to delegate_method(:alt_description).to(:solr_document) }
+  it { is_expected.to delegate_method(:alt_date_created).to(:solr_document) }
+  it { is_expected.to delegate_method(:etd_publisher).to(:solr_document) }
+  it { is_expected.to delegate_method(:degree).to(:solr_document) }
+  it { is_expected.to delegate_method(:advisor).to(:solr_document) }
+  it { is_expected.to delegate_method(:committee_member).to(:solr_document) }
 end
