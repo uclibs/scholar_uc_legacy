94c7ff3161658cff53be9e1109f3564e21c908f1
Olivia Chang <changy6@mail.uc.edu>
2018-06-06 12:21:47 -0400

    delete Lease option in editing file set page

--- spec/features/edit_file_spec.rb
+++ spec/features/edit_file_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-describe "Editing a file:", type: :feature do
+describe "Editing a file:", type: :feature, js: true do
   let(:user) { FactoryBot.create(:user) }
   let(:file_title) { 'Some kind of title' }
   let(:work) { FactoryBot.build(:work, user: user) }
@@ -21,6 +21,14 @@ describe "Editing a file:", type: :feature do
       click_link 'Versions'
       click_button 'Upload New Version'
       expect(page).to have_content "Edit #{file_title}"
+    end
+  end
+
+  context 'when the user is editing the file' do
+    it 'does not show the lease option' do
+      visit edit_hyrax_file_set_path(file_set)
+      click_link 'Permission'
+      expect(page).not_to have_content('Lease')
     end
   end
 end
