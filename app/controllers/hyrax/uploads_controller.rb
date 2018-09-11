84e096847c7ad8361081576e7c92de50fe1ba675
crowesn <crowesn@gmail.com>
2018-09-11 18:35:21 +0000

    Copies uploads model and controller from hyrax

---
+++ app/controllers/hyrax/uploads_controller.rb
@@ -0,0 +1,18 @@
+# Copied from hyrax 366d146 
+
+module Hyrax
+  class UploadsController < ApplicationController
+    load_and_authorize_resource class: Hyrax::UploadedFile
+
+    def create
+      @upload.attributes = { file: params[:files].first,
+                             user: current_user }
+      @upload.save!
+    end
+
+    def destroy
+      @upload.destroy
+      head :no_content
+    end
+  end
+end
---
+++ app/models/hyrax/uploaded_files.rb
@@ -0,0 +1,14 @@
+# Copied from hyrax 366d146
+module Hyrax
+  # Store a file uploaded by a user. Eventually these files get
+  # attached to FileSets and pushed into Fedora.
+  class UploadedFile < ActiveRecord::Base
+    self.table_name = 'uploaded_files'
+    mount_uploader :file, UploadedFileUploader
+    alias uploader file
+    has_many :job_io_wrappers, inverse_of: 'uploaded_file', class_name: 'JobIoWrapper'
+    belongs_to :user, class_name: '::User'
+
+    before_destroy :remove_file!
+  end
+end
